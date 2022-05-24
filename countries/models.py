53ac500b72cc419f46512618ccb1b1a510fe0d57
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-05-25 00:18:11 +0100

    fix: add str to models

--- countries/models.py
+++ countries/models.py
@@ -5,8 +5,14 @@ class Currency(models.Model):
     name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=8)
 
+    def __str__(self):
+        return self.name
+
 
 class Country(models.Model):
     name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=8)
     currencies = models.ManyToManyField(Currency)
+
+    def __str__(self):
+        return self.name
