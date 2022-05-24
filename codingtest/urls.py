ebd521eba5ed69fff126e24e2926033cd66df1f9
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-05-25 00:16:36 +0100

    update: admin site

--- codingtest/urls.py
+++ codingtest/urls.py
@@ -18,7 +18,9 @@ from django.urls import path
 
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
     path("", include("countries.urls")),
+    path('admin/', admin.site.urls),
 ]
--- countries/admin.py
+++ countries/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Currency, Country
+
+admin.site.register(Currency)
+admin.site.register(Country)
