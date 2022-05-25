c3ae155123ecdfa18fd59b1eca69e05448d58414
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-05-26 00:57:28 +0100

    fix: absolute path

--- countries/__tests__/test_graphql.py
+++ countries/__tests__/test_graphql.py
@@ -1,6 +1,7 @@
 import pytest
 from django.test import Client
 import json
+import os
 
 pytestmark = [pytest.mark.django_db]
 
@@ -33,10 +34,11 @@ def test_graphql_countries():
     assert response.json().get("errors") is None
 
     # TODO Make assertions on the returned JSON
-    with open("task_2_expected_output.json", "r+") as f:
+    file_path = (os.path.dirname(__file__)) + "/task_2_expected_output.json"
+    with open(file_path, "r+") as f:
         assert json.loads(f.read()) == response.json()
 
     # TODO Make assertions on the number of database queries
-
+    
 
 # TODO Test querying with the filter as well
