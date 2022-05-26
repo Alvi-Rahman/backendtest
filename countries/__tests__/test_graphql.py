080f46e2803f36c852e769a6ea14b829799e6872
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-05-26 02:03:00 +0100

    add: test_with_aus_filter

--- countries/__tests__/test_graphql.py
+++ countries/__tests__/test_graphql.py
@@ -1,7 +1,10 @@
 import pytest
-from django.test import Client
+from django.test import Client, TestCase
 import json
 import os
+from django.db import connection
+
+from countries.__tests__.test_setup import TestSetUp
 
 pytestmark = [pytest.mark.django_db]
 
@@ -30,6 +33,8 @@ def test_graphql_countries():
         content_type="application/json",
     )
 
+    print(len(connection.queries))
+
     assert response.status_code == 200
     assert response.json().get("errors") is None
 
@@ -39,6 +44,58 @@ def test_graphql_countries():
         assert json.loads(f.read()) == response.json()
 
     # TODO Make assertions on the number of database queries
-    
+    TestCase().assertNumQueries(2)
+
 
 # TODO Test querying with the filter as well
+
+class TestGraphQlCountriesWithFilter(TestSetUp):
+    def test_with_aus_filter(self):
+        response = self.client.post(
+            self.GRAPHQL_URL,
+            {"query": self.aus_query},
+            content_type="application/json",
+        )
+        print(len(connection.queries))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(response.json().get("errors"))
+
+        self.assertEqual(self.aus_filter_response, response.json())
+
+
+
+# def test_graphql_countries_with_filter():
+#
+#     query = """
+#     query {
+#         countries {
+#             name
+#             symbol
+#             currencies {
+#                 name
+#                 symbol
+#             }
+#         }
+#     }
+#     """
+#
+#     client = Client()
+#     response = client.post(
+#         GRAPHQL_URL,
+#         {"query": query},
+#         content_type="application/json",
+#     )
+#
+#     print(len(connection.queries))
+#
+#     assert response.status_code == 200
+#     assert response.json().get("errors") is None
+#
+#     # TODO Make assertions on the returned JSON
+#     file_path = (os.path.dirname(__file__)) + "/task_2_expected_output.json"
+#     with open(file_path, "r+") as f:
+#         assert json.loads(f.read()) == response.json()
+#
+#     # TODO Make assertions on the number of database queries
+#     TestCase().assertNumQueries(2)
